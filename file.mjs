893dc0dfa243d34eebce7f9cc3defcae0d23f5d3
Hiroki Osame <hiroki.osame@gmail.com>
2024-05-13 20:56:21 +0900

    remove space

--- file.mjs
+++ file.mjs
@@ -1,4 +1,4 @@
-import assert from "assert";const startTime = performance.now();console.log("start time:", performance.now() - startTime); try {assert.ok(false)}finally{console.log("end time:",performance.now()-startTime)}
+import assert from "assert";const startTime = performance.now();console.log("start time:", performance.now() - startTime);try {assert.ok(false)}finally{console.log("end time:",performance.now()-startTime)}
 /*
  
  
